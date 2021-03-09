b34493351bac6b80bcef438bde74f709c98edb09
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-09 20:03:58 +0600

    add comment length

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -4,18 +4,28 @@ import { useParams } from 'react-router';
 const PostDetail = () => {
   const {id} = useParams()
   const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
   useEffect(() => {
     const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
     fetch(url)
     .then(res => res.json())
     .then(data => setPost(data));
   }, [])
+
+  useEffect(() => {
+    const url= `https://jsonplaceholder.typicode.com/comments?postId=${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setComments(data))
+  },[])
+
   return (
     <div>
       <h3>This is post detail: {id}</h3>
       <p>User Posted: {post.id}</p>
       <p>Title: {post.title}</p>
       <p>Poste body: {post.body}</p>
+      <p>Number of comments: {comments.length}</p>
     </div>
   );
 };
