c915402d3614beb00b7001a719b636e60139d673
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-09 19:55:23 +0600

    post info

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const PostDetail = () => {
   const {id} = useParams()
+  const [post, setPost] = useState({});
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setPost(data));
+  }, [])
   return (
     <div>
       <h3>This is post detail: {id}</h3>
+      <p>User Posted: {post.id}</p>
+      <p>Title: {post.title}</p>
+      <p>Poste body: {post.body}</p>
     </div>
   );
 };
