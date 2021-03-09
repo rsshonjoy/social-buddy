989e8f39a55e420cfd08141e76bb544f882ca83c
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-09 19:42:34 +0600

    dynamic post

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const PostDetail = () => {
+  const {id} = useParams()
   return (
     <div>
-      <h3>This is post detail</h3>
+      <h3>This is post detail: {id}</h3>
     </div>
   );
 };
